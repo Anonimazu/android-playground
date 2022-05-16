f80bb38ba6077dfe90f700eb81116f37d634c5c9
Anonimazu <monthon.chat@gmail.com>
2022-05-16 20:47:22 +0700

    Add motion layout

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.androidplayground
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidplayground.animation.AnimationActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialView()
+    }
+    private fun initialView() {
+        btnOpenAnimationSession.setOnClickListener {
+            startActivity(Intent(this, AnimationActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidplayground/animation/AnimationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidplayground.animation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidplayground.R
+
+class AnimationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation)
+    }
+}
